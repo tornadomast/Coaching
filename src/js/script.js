c59a4055c7da3dbc46b7d7cdd3c26c6901404348
Oleksii <tornado@gmail.com>
2022-10-05 23:21:26 +0300

    create block sliders

--- src/js/script.js
+++ src/js/script.js
@@ -9,18 +9,18 @@ menu.addEventListener('click', () => {
     menu.classList.remove('active')
 });
 
-// $(document).ready(function(){
-//     $('.promo__carousel').slick({
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 2500,
-//         dots: false,
-//         infinite: true,
-//         speed: 700,
-//         fade: true,
-//         arrows: false,
-//         adaptiveHeight: true,
-//         cssEase: 'linear'
-//       });
-//   });
+$(document).ready(function(){
+    $('.slider__carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        dots: false,
+        infinite: true,
+        speed: 700,
+        fade: true,
+        arrows: false,
+        adaptiveHeight: true,
+        cssEase: 'linear'
+      });
+  });
