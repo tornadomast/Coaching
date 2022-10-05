fbe9a361f20f860bf4d76a385fd2e42f4f265409
Oleksii <tornado@gmail.com>
2022-10-05 14:31:53 +0300

    adaptiv first block

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,26 @@
-"use strict";
+"use sctrict";
+const hamburger = document.querySelector('.navbar__hamburger'),
+        menu = document.querySelector('.menu');
 
-$(document).ready(function(){
-    $('.promo__carousel').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        dots: false,
-        infinite: true,
-        speed: 700,
-        fade: true,
-        arrows: false,
-        adaptiveHeight: true,
-        cssEase: 'linear'
-      });
-  });
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active')
+});
+menu.addEventListener('click', () => {
+    menu.classList.remove('active')
+});
+
+// $(document).ready(function(){
+//     $('.promo__carousel').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2500,
+//         dots: false,
+//         infinite: true,
+//         speed: 700,
+//         fade: true,
+//         arrows: false,
+//         adaptiveHeight: true,
+//         cssEase: 'linear'
+//       });
+//   });
