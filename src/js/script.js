92ff39c6911ebae70aab2a3253404f94049aaa68
Oleksii <tornado@gmail.com>
2022-10-08 00:25:56 +0300

    created block card and adaptive him

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,52 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
 });
 
+// const detalBtn = document.querySelector('.card__detals'),
+//     info = document.querySelector('.box-info__content'),
+//     box = document.querySelector('.box-info__list'),
+//     come = document.querySelector('.box-info__back');
+
+// detalBtn.addEventListener('click', () => {
+//     info.classList.add('active');
+//     box.classList.add('active');
+// });
+// come.addEventListener('click', () => {
+//     info.classList.remove('active');
+//     box.classList.remove('active');
+// });
+
+
+// $('.card__detals').each(function(i) {
+//     $(this).on('click', function(e) {
+//         e.preventDefault();
+//         $('.box-info__content').eq(i).toggleClass('box-info__content_active');
+//         $('.box-info__list').eq(i).toggleClass('active');
+//     });
+// });
+// $('.box-info__back').each(function(i) {
+//     $(this).on('click', function(e) {
+//         e.preventDefault();
+//         $('.box-info__content').eq(i).removeClass('box-info__content_active');
+//         $('.box-info__list').eq(i).removeClass('active');
+//     });
+// });
+
+$('.item-card__detal').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.item-card').eq(i).toggleClass('item-card_active');
+            $('.item-info').eq(i).toggleClass('active');
+        });
+    });
+$('.item-info__back').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.item-card').eq(i).removeClass('item-card_active');
+            $('.item-info').eq(i).removeClass('active');
+        });
+    });
+
+
 
 $(document).ready(function(){
     $('.slider__carousel').slick({
@@ -19,7 +65,8 @@ $(document).ready(function(){
         speed: 300,
         fade: true,
         arrows: false,
-        adaptiveHeight: true,
         cssEase: 'linear'
       });
+
+      
   });
